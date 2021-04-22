3a90acf73a52593157e35ac8a480b893bd151b3d
Gaeel Bradshaw <gaeel.bradshaw@goto-games.com>
2021-04-22 18:53:15 +0200

    Enable posting to slack

--- index.js
+++ index.js
@@ -1,17 +1,31 @@
+const fs = require('fs');
+const path = require('path');
 const core = require('@actions/core');
-const github = require('@actions/github');
-
+// const github = require('@actions/github');
+//
 const { WebClient } = require('@slack/web-api');
 
 try {
-  // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput('who-to-greet');
-  console.log(`Hello ${nameToGreet}!`);
-  const time = (new Date()).toTimeString();
-  core.setOutput("time", time);
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
+  const web = new WebClient(core.getInput('SLACK_BOT_TOKEN'));
+  let channel = core.getInput('channel')
+  let messageJSON = fs.readFileSync(path.join('.github', 'workflows', core.getInput('message')), 'utf-8');
+  let ghregx = /\${{\s?GITHUB_([A-Z_]*)\s?}}/gm
+
+  messageJSON = messageJSON.replace(ghregx, (_, env) => process.env[`GITHUB_${env}`]);
+
+  let message = JSON.parse(messageJSON);
+  (async () => {
+
+    try {
+      // Use the `chat.postMessage` method to send a message from this app
+      await web.chat.postMessage(message);
+    } catch (error) {
+      console.log(error);
+    }
+
+    console.log('Message posted!');
+  })();
+
 } catch (error) {
   core.setFailed(error.message);
 }
