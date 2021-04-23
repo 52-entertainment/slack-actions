07391baca9fb6092ec526c275464e314bf7149b5
Gaeel Bradshaw <gaeel.bradshaw@goto-games.com>
2021-04-23 13:37:50 +0200

    Replace all environment tags

--- index.js
+++ index.js
@@ -1,29 +1,33 @@
 const fs = require('fs');
 const path = require('path');
+
 const core = require('@actions/core');
-// const github = require('@actions/github');
-//
+
 const { WebClient } = require('@slack/web-api');
 
 try {
-  const web = new WebClient(core.getInput('SLACK_BOT_TOKEN'));
-  let channel = core.getInput('channel')
-  let messageJSON = fs.readFileSync(path.join('.github', 'workflows', core.getInput('message')), 'utf-8');
-  let ghregx = /\${{\s?GITHUB_([A-Z_]*)\s?}}/gm
 
-  messageJSON = messageJSON.replace(ghregx, (_, env) => process.env[`GITHUB_${env}`]);
+  const web = new WebClient(core.getInput('SLACK_BOT_TOKEN'));
+  let messagePath = core.getInput('message');
+  let messageJSON;
+  if (fs.existsSync(messagePath)){
+    messageJSON = fs.readFileSync(messagePath, 'utf-8');
+  } else {
+    messageJSON = messagePath;
+  }
+  let envregx = /\${{\s?([A-Z_]*)\s?}}/gm;
+
+  messageJSON = messageJSON.replace(envregx, (_, env) => process.env[env]);
 
   let message = JSON.parse(messageJSON);
+
   (async () => {
 
     try {
-      // Use the `chat.postMessage` method to send a message from this app
       await web.chat.postMessage(message);
     } catch (error) {
-      console.log(error);
+      core.setFailed(error.message);
     }
-
-    console.log('Message posted!');
   })();
 
 } catch (error) {
